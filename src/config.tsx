ff93e16cf3050682b4fd4b452a0610d8d25373b4
Luka Cheuk <winglok0416@gmail.com>
2023-03-14 16:41:43 +0800

    some env

--- src/config.tsx
+++ src/config.tsx
@@ -1,7 +1,7 @@
 const config = {
     backendUrl: process.env.REACT_APP_BACKEND_URL || "http://localhost",
-    authUsername: process.env.REACT_APP_BACKEND_AUTH_USERNAME || "",
-    authPassword: process.env.REACT_APP_BACKEND_AUTH_PASSWORD || "",
+    authUsername: process.env.REACT_APP_BACKEND_AUTH_USERNAME || "advisor",
+    authPassword: process.env.REACT_APP_BACKEND_AUTH_PASSWORD || "f7cd82c8-ac20-4380-8977-07bdb0f291cf",
 }
 
 export default config;
